cce341679477e64c559a6e4847f0ebd9f9a45c69
soneralp <soneralp.sayinn@gmail.com>
2024-05-19 16:19:48 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Head from './components/Head.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <Head msg="Basic Form App" />
+      <MyForm />
+    </div>
+  </header>
+
+</template>
+
+<script>
+import MyForm from './components/Form.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyForm
+  }
+};
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="form">
+      <form @submit.prevent="addItem">
+        <input v-model="newItem" placeholder="Add new value">
+        <button class="custom-button" type="submit">Add</button>
+      </form>
+
+      <ul>
+        <li v-for="(item, index) in items" :key="index">
+          {{ item }}
+          <button class="custom-button" @click="removeItem(index)">Delete</button>
+        </li>
+    </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newItem: '',
+        items: []
+      };
+    },
+    methods: {
+      addItem() {
+        if (this.newItem !== '') {
+          this.items.push(this.newItem);
+          this.newItem = '';
+        } else {
+            alert('Please enter a valid value');
+        }
+      },
+      removeItem(index) {
+        this.items.splice(index, 1);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  form {
+    margin-bottom: 20px;
+  }
+  input {
+    margin-right: 10px;
+  }
+  button {
+    margin-left: 10px;
+  }
+  </style>
+  
